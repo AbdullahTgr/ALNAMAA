dea5094be6f58da6937c3e68275c94b6d7d66ac1
AbdullahTgr <4multijob@gmail.com>
2022-05-20 16:36:42 +0200

    gf

--- resources/views/frontend/pages/blog.blade.php
+++ resources/views/frontend/pages/blog.blade.php
@@ -9,7 +9,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     
-	<meta property="og:url" content="https://www.facebook.com/alsaeedyblog"> 
+	<meta property="og:url" content="https://www.facebook.com/companynama"> 
 
     
 	<meta name="keywords" content="@foreach($tags as $tag),{{ $tag->title}}@endforeach">
